25d1e9a813a80e6a798c21b41dabd0baa37519f9
Mountain311 <91728136+Mountain311@users.noreply.github.com>
2021-12-20 16:18:29 +0500

    full task

--- task1.py
+++ task1.py
@@ -9,9 +9,34 @@ def encrypt(text,s):
     return result
 
 
-text = "CEASER CIPHER DEMO"
-s = 4
+def decrypt(message):
 
-print ("Plain Text : " + text)
-print ("Shift pattern : " + str(s))
+    LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    for key in range(len(LETTERS)):
+        translated = ''
+        for symbol in message:
+            if symbol in LETTERS:
+                num = LETTERS.find(symbol)
+                num = num - key
+                if num < 0:
+                    num = num + len(LETTERS)
+                translated = translated + LETTERS[num]
+            else:
+                translated = translated + symbol
+
+        print('Hacking key #%s: %s' % (key, translated))
+
+
+text = input("Enter Text to Encrypt:")
+s = int(input("Enter Shift Key:"))
+
+print()
+
 print ("Cipher: " + encrypt(text,s))
+
+print('\n\n')
+
+detext = input("Enter Text to Decrypt")
+
+decrypt(detext)
